a92e8f612cd15f3eb9cb79d7e82e702547249371
chihaoyo <chihaoyo@gmail.com>
2020-04-02 13:42:13 +0800

    Simplify urls_to_sites
    Use `/channel/[id]` url for youtube_channel & `/user/[id]` url for youtube_user

--- urls-to-sites.mjs
+++ urls-to-sites.mjs
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import { datetime, pause } from './lib/util.mjs'
-import { parseURL, getChannel, getUser } from './lib/yt.mjs'
+import { TYPE, parseURL, getChannel, getUser } from './lib/yt.mjs'
 
 function writeToJSONFile(val, fileName) {
   fs.writeFile(fileName, JSON.stringify(val), err => {
@@ -20,28 +20,33 @@ async function list(urls) {
   let channels = []
   let users = []
   for(let url of urls) {
+    console.log(url)
     let { type, id } = parseURL(url)
-    if(type === 'channel' && GET_CHANNEL) {
+    if(type === TYPE.channel && GET_CHANNEL) {
       let channel = await getChannel(id, part)
       if(channel) {
-        channels.push(channel)
-        writeToJSONFile(channels, 'data/channels-' + datetimeStr + '.json') // FIXME: brute force continuous file write
+        channels.push({
+          name: channel.snippet.title,
+          url: url
+        })
       }
       await pause()
-    } else if(type === 'user' && GET_USER) {
+    } else if(type === TYPE.user && GET_USER) {
       let user = await getUser(id, part)
       if(user) {
-        users.push(user)
-        writeToJSONFile(users, 'data/users-' + datetimeStr + '.json') // FIXME: brute force continuous file write
+        users.push({
+          name: user.snippet.title,
+          url
+        })
       }
       await pause()
     }
   }
   for(let channel of channels) {
-    console.log(channel.snippet.title + '\t' + 'https://www.youtube.com/channel/' + channel.id)
+    console.log(channel.name + '\t' + channel.url)
   }
   for(let user of users) {
-    console.log(user.snippet.title + '\t' + 'https://www.youtube.com/channel/' + user.id)
+    console.log(user.name + '\t' + user.url)
   }
 }
 
