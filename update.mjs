c4bbe82fed2a7a410d24554172ffd7db55063ff4
chihaoyo <chihaoyo@gmail.com>
2020-04-11 17:13:22 +0800

    Add user intervention to update

--- update.mjs
+++ update.mjs
@@ -2,8 +2,45 @@ import { pause, datetime } from './lib/util.mjs'
 import * as youtube from './lib/yt.mjs'
 import { YT, MYSQL } from './config.mjs'
 import mysql from 'mysql2/promise'
+import inquirer from 'inquirer'
 
 const MAX_ARTICLE_UPDATE = 10 // round robin
+
+let ask = true
+let ANSWERS = {
+  yes: 'yes',
+  stopAsking: 'stopAsking',
+  skip: 'skip',
+  abort: 'abort',
+}
+async function prompt() {
+  let res = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'input',
+      message: 'Go?',
+      choices: [
+        {
+          name: 'Yes.',
+          value: ANSWERS.yes
+        },
+        {
+          name: 'Stop asking.',
+          value: ANSWERS.stopAsking
+        },
+        {
+          name: 'Skip this site.',
+          value: ANSWERS.skip
+        },
+        {
+          name: 'Abort.',
+          value: ANSWERS.abort
+        }
+      ]
+    }
+  ])
+  return res.input
+}
 
 async function updateArticlesOfSite(site, pool) {
   let date = new Date()
@@ -12,6 +49,23 @@ async function updateArticlesOfSite(site, pool) {
   let [rows] = await pool.query('SELECT * FROM Article WHERE site_id = ? AND next_snapshot_at != 0 AND next_snapshot_at <= ? ORDER BY next_snapshot_at ASC, article_id ASC LIMIT ?', [site.site_id, timestamp, MAX_ARTICLE_UPDATE])
   let articles = rows
   console.log('update', articles.length, 'articles of site', site.site_id)
+  if(articles.length < 1) {
+    return
+  }
+  console.log('articles', articles.map(a => a.article_id).join(' '))
+
+  let answer = ANSWERS.yes
+  if(ask) {
+    answer = await prompt()
+  }
+  if(answer === ANSWERS.stopAsking) {
+    ask = false
+  } else if(answer === ANSWERS.skip) {
+    return
+  } else if(answer === ANSWERS.abort) {
+    process.abort()
+  }
+
   for(let article of articles) {
     console.log('update article', article.article_id, article.url)
     date = new Date()
